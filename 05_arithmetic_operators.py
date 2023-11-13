bb5f6b39e998c092231b5f98f559272df493abee
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-11-13 22:45:48 +0330

    arithmetic operators

---
+++ 05_arithmetic_operators.py
@@ -0,0 +1,12 @@
+number_1, number_2 = 100, 10
+result = number_1 + number_2
+print(result)
+
+x, y = number_1, number_2
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+print(x / y)  # Division
+print(x // y)  # Floor division
+print(x * y)  # Multiplication
+print(x % y)  # Modulus
+print(x ** y)  # Exponentiation/Power
