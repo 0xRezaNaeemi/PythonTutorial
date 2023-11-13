c49fad1942d31164d1c1eafd06278dd10f744b0a
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-11-13 22:44:32 +0330

    identifier

---
+++ 04_identifier.py
@@ -0,0 +1,21 @@
+# StartWith A-Z, a-z and underscore(_)
+x = 5
+X = 3
+_ali_ = 25
+
+# snake_case
+my_variable_name = X
+
+
+# camelCase or snake_case
+def binarySearch():
+    pass
+
+
+def binary_search():
+    pass
+
+
+# PascalCase
+class MyClass:
+    pass
