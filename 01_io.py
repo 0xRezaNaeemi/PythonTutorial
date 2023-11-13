3af15b0105713c61037a4def52ec5c83d0a7b07e
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-11-13 22:42:12 +0330

    input/ouput

---
+++ 01_io.py
@@ -0,0 +1,24 @@
+# input
+your_name = input("Enter your name: ")
+# output
+print("your name is: ", your_name)
+
+# print values
+print(2.5, 5, 'ali', True, False)
+
+# variables
+float_number = 2.5
+integer_number = 5
+string_name = "Ali"
+is_ok = True
+is_not_ok = False
+
+# print variable values
+print(float_number, integer_number,
+      string_name, is_ok, is_not_ok)
+
+# escape characters
+print(1, 2, 3, sep=": ")
+print(1, 2, 3, sep=", ")
+print(1, 2, 3, end="*\n")
+print("1\n2\tali\n")
