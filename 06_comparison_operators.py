7d3e283865c922718d632ac723d76752da7a8a0e
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-11-13 22:46:28 +0330

    comparison operators

---
+++ 06_comparison_operators.py
@@ -0,0 +1,12 @@
+x, y = 4, 3
+
+print(x == y)  # Equal
+print(x != y)  # Not equal
+print(x > y)  # Greater than
+print(x < y)  # Less than
+print(x >= y)  # greater than or equal to
+print(x <= y)  # Less than or equal to
+
+z = 8
+print(x == y != z)
+print(x >= y <= z)
