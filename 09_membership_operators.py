7878e1620a9ece30cdf3c7a67be49b38d1faae1a
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-11-13 22:48:20 +0330

    membership operators

---
+++ 09_membership_operators.py
@@ -0,0 +1,9 @@
+# Membership Operators
+
+my_sentence = "In the name of almighty Allah"
+print("g" in my_sentence)
+print("g" not in my_sentence)
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(5 in my_list)
+print(7 not in my_list)
