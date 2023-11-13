c4c1971ec1baf32e3af461e82d13bb226f1cc91a
Reza Naeemi <RezaNaeemiRad@gmail.com>
2023-11-13 23:57:58 +0330

    operator precedence

---
+++ S01_Basics/13_operator_precedence.py
@@ -0,0 +1,22 @@
+"""
+Operator Precedence:
+1. Lambda Expression: lambda
+2. Boolean OR: or
+3. Boolean AND: and
+4. Boolean NOT: not
+5. Membership, Comparison: in, not in, is, not is, <, <=, >, >=, ==, !=
+6. Bitwise OR: |
+7. Bitwise XOR: ^
+8. Bitwise AND: &
+9. Bitwise Left/Right shift: <<, >>
+10. Add, Sub: +, -
+11. Mul, Div, Mod: *, /, %
+12. Unary Plus/Minus: +, -
+13. Bitwise NOT: ~
+14. Exponential: **
+15. Array Subscription: []
+16. Slicing: [index:index]
+17. Function Call: f()
+18. Grouping:  ()
+"""
+print(2 * 5 / 2 ** 4 - 2 + 4)
